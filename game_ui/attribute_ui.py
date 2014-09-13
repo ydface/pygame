1a0a2c662575e78494f632dde4d820fadc93cb8e
Ydface <329302080@qq.com>
2014-09-13 10:32:26 +0800

    ui windows move able

--- game_ui/attribute_ui.py
+++ game_ui/attribute_ui.py
@@ -20,6 +20,24 @@ class AttributeUI(util.node.Node):
         util.node.Node.__init__(self, zorder)
 
         self.image = resource.getImage("attribute")
+        self.move_able = False
+        self.rect = Rect(100, 300, self.image.get_width(), self.image.get_height())
 
     def draw_self(self):
-        screen.blit(self.image, (100, 300))
+        screen.blit(self.image, (self.rect[0], self.rect[1]))
+
+    def handle_event(self, event):
+        if event.type == MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
+            position = pygame.mouse.get_pos()
+            if self.rect.collidepoint(position):
+                self.move_able = True
+        elif event.type == MOUSEMOTION:
+            position = pygame.mouse.get_pos()
+            if self.rect.collidepoint(position):
+                rel = pygame.mouse.get_rel()
+                if self.move_able:
+                    self.rect[0] += rel[0]
+                    self.rect[1] += rel[1]
+
+        elif event.type == MOUSEBUTTONUP:
+            self.move_able = False
--- game_ui/bag_ui.py
+++ game_ui/bag_ui.py
@@ -19,17 +19,17 @@ class Item(util.node.Node):
     def __init__(self, father, item_id, pos):
         util.node.Node.__init__(self)
 
+        self.father = father
         self.item_id = item_id
         self.image = resource.getImage("item_" + str(self.item_id))
         self.index = pos
 
-        x = pos % 6
-        y = pos / 6
-
-        self.pos = [father.pos[0] + 37.5 * x + 5 + 6 * (x + 1), father.pos[1] + 37.5 * y + 15 + 15 * y]
+        self.x = pos % 6
+        self.y = pos / 6
 
     def draw_self(self):
-        screen.blit(self.image, self.pos)
+        pos = (self.father.rect[0] + 37.5 * self.x + 5 + 6 * (self.x + 1), self.father.rect[1] + 37.5 * self.y + 15 + 15 * self.y)
+        screen.blit(self.image, pos)
 
 
 class BagUI(util.node.Node):
@@ -39,13 +39,30 @@ class BagUI(util.node.Node):
         image = resource.getImage("bag_background")
         self.image = pygame.transform.scale(image, (image.get_width() * 2 / 3, image.get_height() * 2))
 
-        self.pos = (280, 300)
+        self.move_able = False
+        self.rect = Rect(280, 300, self.image.get_width(), self.image.get_height())
 
         self.items = dict()
         for i in range(0, 30):
             self.items[str(i)] = Item( self, i + 1, i)
 
     def draw_self(self):
-        screen.blit(self.image, self.pos)
+        screen.blit(self.image, (self.rect[0], self.rect[1]))
         for i in self.items:
             self.items[i].draw_self()
+
+    def handle_event(self, event):
+        if event.type == MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
+            position = pygame.mouse.get_pos()
+            if self.rect.collidepoint(position):
+                self.move_able = True
+        elif event.type == MOUSEMOTION:
+            position = pygame.mouse.get_pos()
+            if self.rect.collidepoint(position):
+                rel = pygame.mouse.get_rel()
+                if self.move_able:
+                    self.rect[0] += rel[0]
+                    self.rect[1] += rel[1]
+
+        elif event.type == MOUSEBUTTONUP:
+            self.move_able = False
